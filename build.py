3e765fabf39e4b65711bcac4723c1ddea9992bed
zmbksyyr <zmbksy25@gmail.com>
2022-08-07 09:31:08 +0800

    Update build.py

--- build.py
+++ build.py
@@ -8,8 +8,6 @@ gfw_url = 'https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/releas
 greatfire_url = 'https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/greatfire.txt'
 telegram_url = 'https://core.telegram.org/resources/cidr.txt'
 
-ipv6 = '''(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'''
-
 ipv4 = '''((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'''
 
 def get_rule(rules_url):
@@ -44,6 +42,7 @@ def clear_format(rule):
 def getRulesStringFromFile(allrules, kind):
     ret = ''
     for rule in allrules:
+	kindA = kind
         rule = rule.strip('\r\n')
         if not len(rule):
             continue
@@ -51,17 +50,18 @@ def getRulesStringFromFile(allrules, kind):
         if rule.startswith('#'):
             ret += rule + '\n'
         else:
-            prefix = 'DOMAIN-SUFFIX'
-            if re.match(ipv4, rule):
-                prefix = 'IP-CIDR'
-                if '/' not in rule:
-                    rule += '/32'
-            elif re.match(ipv6, rule):
-                prefix = 'IP-CIDR'
-            elif '.' not in rule and len(rule) > 1:
-                prefix = 'DOMAIN-KEYWORD'
-
-            ret += prefix + ',%s,%s\n' % (rule, kind)
+#             prefix = 'DOMAIN-SUFFIX'
+#             if rule.count('.') == 1:
+# 		prefix = 'DOMAIN'
+#             elif re.match(ipv4, rule) or rule.count(':') != 0:
+#                 prefix = 'IP-CIDR'
+#                 if '/' not in rule:
+#                     rule += '/32'
+#                 kindA += ",no-reso"
+#             elif '.' not in rule and len(rule) > 1:
+#                 prefix = 'DOMAIN-KEYWORD'
+            prefix = 'RULE-SET'
+            ret += prefix + ',%s,%s\n' % (rule, kindA)
 
     return ret
 
