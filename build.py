e9de8f3ecb2f425291a0d6711a18eb1b5434caf7
zmbksyyr <zmbksy25@gmail.com>
2022-08-26 09:43:59 +0800

    Update build.py

--- build.py
+++ build.py
@@ -39,30 +39,6 @@ def clear_format(rule):
 
     return rules
 
-# def getRulesStringFromFile(allrules, kind):
-#     ret = ''
-#     for rule in allrules:
-#         rule = rule.strip('\r\n')
-#         if not len(rule):
-#             continue
-
-#         if rule.startswith('#'):
-#             ret += rule + '\n'
-#         else:
-#             prefix = 'DOMAIN-SUFFIX'
-#             if rule.count('.') == 1:
-#                 prefix = 'DOMAIN'
-#             elif re.match(ipv4, rule) or rule.count(':') != 0:
-#                 prefix = 'IP-CIDR'
-#                 if '/' not in rule:
-#                     rule += '/32'
-#             elif '.' not in rule and len(rule) > 1:
-#                 prefix = 'DOMAIN-KEYWORD'
-
-#             ret += prefix + ',%s,%s\n' % (rule, kind)
-
-#     return ret
-
 def getRulesStringFromFile(allrules, kind):
     ret = ''
     for rule in allrules:
@@ -73,8 +49,10 @@ def getRulesStringFromFile(allrules, kind):
         if rule.startswith('#'):
             ret += rule + '\n'
         else:
-            prefix = 'RULE-SET'
-            if re.match(ipv4, rule) or rule.count(':') != 0:
+            prefix = 'DOMAIN-SUFFIX'
+            if rule.count('.') == 1:
+                prefix = 'DOMAIN'
+            elif re.match(ipv4, rule) or rule.count(':') != 0:
                 prefix = 'IP-CIDR'
                 if '/' not in rule:
                     rule += '/32'
@@ -84,7 +62,6 @@ def getRulesStringFromFile(allrules, kind):
             ret += prefix + ',%s,%s\n' % (rule, kind)
 
     return ret
-
 
 # main
 
